d3b88ed323a27a02d7719b02cd0db057c1a7dd24
Louis King <jinglemansweep@gmail.com>
2011-11-16 17:56:31 +0000

    Refactoring/Tidying

--- mhub/service.py
+++ mhub/service.py
@@ -41,7 +41,6 @@ class BaseService(Service):
         self.reactor = reactor
         self.logger = logging.getLogger("mhub.service")
         self.cfg = cfg
-        self.factories = dict()
         self.plugins = dict()
 
 
@@ -51,38 +50,7 @@ class BaseService(Service):
         Setup service.
         """
 
-        self.setup_reactor()
-        
-
-    # Reactor handling
-
-    def setup_reactor(self):
-
-        """
-        Setup Twisted reactor.
-        """
-
-        print "setup reactor"
-
-        for plugin_name, plugin_inst in self.plugins.iteritems():
-            if not hasattr(plugin_inst, "tasks"): continue
-            plugin_tasks = plugin_inst.tasks
-            print plugin_tasks
-            for plugin_task in plugin_tasks:
-                interval = plugin_task[0]
-                print plugin_task
-                func = plugin_task[1]
-                self.logger.debug("Registered '%s' from '%s' every %.2f seconds" % (func.__name__,
-                                                                          plugin_name,
-                                                                          interval))
-                def blocking_call(func, interval):
-                    d = threads.deferToThread(func)
-                    reactor.callLater(interval, blocking_call, func, interval)
-                    return d
-
-                d = blocking_call(func, interval)
-
-            self.logger.info("%i tasks declared" % (len(plugin_tasks)))
+        pass
 
 
     def install_plugin(self, name, plugin):
